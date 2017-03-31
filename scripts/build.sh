38e14cedbaa2b34163c6057815c33d3ecab185e1
kenstin <k3nstin@gmail.com>
2017-03-31 15:23:46 +0200

    Add unit testing

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,7 +15,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "\e[36m\e[1mBuild triggered for PR #${TRAVIS_PULL_REQUEST} to branch\"${TRAVIS_BRANCH}\""
 fi
 
-#nuget sources Add -Name "Discord.NET 1.0" -Source "https://www.myget.org/F/discord-net/api/v3/index.json"
 dotnet restore
-#dotnet test
-dotnet publish -c Release
+dotnet test MakiseSharp.Tests/MakiseSharp.Tests.csproj
+dotnet publish MakiseSharp/MakiseSharp.csproj -c Release
