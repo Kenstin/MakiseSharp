4bbb8a934351585e7316a08a5bc3917d66678dfd
Unknown <k3nstin@gmail.com>
2018-02-26 13:30:09 +0100

    Deploy images through ssh using docker save

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-cd MakiseSharp/bin/Release/netcoreapp1.1
-rm -R publish/runtimes/win
-rm publish/*.pdb
-
-cd publish/ && tar czf ../package.tgz . && cd .. 
-
 export SSHPASS=$DEPLOY_PASS
 echo "Sending the package..."
-sshpass -e scp -o stricthostkeychecking=no package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+
+docker save makisekurisu | bzip2 | pv | \
+/usr/bin/sshpass -e -v ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST 'bunzip2 | docker load' 
+
 sleep 3
-sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/deploy.sh
+sshpass -e -v ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/deploy.sh
