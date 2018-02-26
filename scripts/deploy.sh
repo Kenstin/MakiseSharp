1543a2411241604f85330a76340ceb45431d60ff
Hebiame <19415637+Hebiame@users.noreply.github.com>
2018-02-26 14:21:20 +0100

    Delete -v from sshpass

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ export SSHPASS=$DEPLOY_PASS
 echo "Sending the package..."
 
 docker save makisekurisu | bzip2 | pv | \
-/usr/bin/sshpass -e -v ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST 'bunzip2 | docker load' 
+/usr/bin/sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST 'bunzip2 | docker load' 
 
 sleep 3
-sshpass -e -v ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/deploy.sh
+sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/deploy.sh
